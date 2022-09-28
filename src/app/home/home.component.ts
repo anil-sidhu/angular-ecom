e9a4f474b0302719280fe120c5d848c45dcaec36
anil sidhu <webanilsidhu@gmail.com>
2022-09-28 21:49:58 +0530

    Done with dynamic crasuel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { product } from '../data-type';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-  constructor() {}
+ popularProducts:undefined|product[];
+  constructor(private product:ProductService) {}
 
   ngOnInit(): void {
+    this.product.popularProducts().subscribe((data)=>{
+      this.popularProducts=data;
+    })
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -26,4 +26,8 @@ export class ProductService {
    updateProduct(product:product){
     return this.http.put<product>(`http://localhost:3000/products/${product.id}`,product)
    }
+   popularProducts(){
+    return this.http.get<product[]>("http://localhost:3000/products?_limit=3");
+   }
+   
 }
