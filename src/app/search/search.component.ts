886c3c7338194d2fd2cf055ead89fca25da74459
anil sidhu <webanilsidhu@gmail.com>
2022-10-04 15:08:21 +0530

    Done with search api

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { product } from '../data-type';
+import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  searchResult:undefined|product[]
+  constructor(private activeRoute: ActivatedRoute, private product:ProductService) { }
 
   ngOnInit(): void {
+    let query = this.activeRoute.snapshot.paramMap.get('query');
+    console.warn(query);
+    query && this.product.searchProduct(query).subscribe((result)=>{
+      this.searchResult=result;
+      
+    })
+    
+
   }
 
 }
