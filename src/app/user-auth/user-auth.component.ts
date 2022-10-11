79ca08f98285119442e3cee90197b47c1931a16e
anil sidhu <webanilsidhu@gmail.com>
2022-10-11 19:02:30 +0530

    Done with login UI

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { signUp } from '../data-type';
+import { login, signUp } from '../data-type';
 import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-auth',
   templateUrl: './user-auth.component.html',
-  styleUrls: ['./user-auth.component.css']
+  styleUrls: ['./user-auth.component.css'],
 })
 export class UserAuthComponent implements OnInit {
-
-  constructor(private user : UserService) { }
+  showLogin:boolean=true
+  constructor(private user: UserService) {}
 
   ngOnInit(): void {
     this.user.userAuthReload();
   }
 
-  signUp(data:signUp){
-
+  signUp(data: signUp) {
     this.user.userSignUp(data);
   }
-
+  login(data: login) {
+    console.warn(data);
+  }
+  openSignUp(){
+    this.showLogin=false
+  }
+  openLogin(){
+this.showLogin=true;
+  }
 }
